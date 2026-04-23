dcfbe035d7ae5b575f869fc52c5cb3bc64de651e
Sushant Goyal <goyalsushant2@gmail.com>
2026-04-23 10:31:11 +0530

    Added promises and async files

--- adv-js/pod.js
+++ adv-js/pod.js
@@ -42,15 +42,30 @@ getUserDetails(data, (err, data) => {
 })
 
 getUserDetails(data)
-.then(user => {
-    console.log(user)
-    return getPosts(user.id)
-})
-.then(posts => {
-    console.log(posts)
-    return getComments(posts.id)
-})
-.then(comments => {
-    console.log(comments)
-})
-.catch(err => console.error(err))
+    .then(user => {
+        console.log(user)
+        return getPosts(user.id)
+    })
+    .then(posts => {
+        console.log(posts)
+        return getComments(posts.id)
+    })
+    .then(comments => {
+        console.log(comments)
+    })
+    .catch(err => console.error(err))
+
+
+async function fetchData() {
+    try {
+        const user = await getUserDetails(1) // Wait till user details are received or an error occurs
+        console.log(user)
+        const posts = await getPosts(user.id) // wait till user posts are recevied or an error occurs
+        console.log(posts)
+        const comments = await getComments(posts.id) // wait till post comments are received or an error occurs
+        console.log(posts)
+    }
+    catch (err) {
+        console.log('Error', err)
+    }
+}
